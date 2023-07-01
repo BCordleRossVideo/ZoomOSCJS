5129458fd4e33e4a2e17b0bbe73ae5ab97f77d59
Bo <bcordle@rossvideo.com>
2023-07-01 00:16:27 -0400

    Added Rosstalk.ts

---
+++ src/Rosstalk.ts
@@ -0,0 +1,125 @@
+'use strict';
+
+var net = require('net');
+var util = require('util');
+var events = require('events');
+
+const cr = '\r\n';
+
+/**
+ *  Expose `Rosstalk`
+ */
+
+module.exports = Rosstalk;
+
+/**
+ *  A talkable ross
+ *
+ *  @params {Object} opts
+ */
+
+function Rosstalk(opts) {
+  opts = opts || {};
+
+  if (!opts.host) throw new Error('Must provide switcher host address');
+
+  this.host = opts.host;
+  this.port = opts.port || 7788;
+
+  this.connection = null;
+};
+
+/**
+ *  Inherit the EE
+ */
+
+util.inherits(Rosstalk, events.EventEmitter);
+
+/**
+ *  Connect to the switcher
+ */
+
+Rosstalk.prototype.connect = function connect() {
+    this.connection = net.connect(this.port, this.host);
+    
+    this.connection.on('connect', () => {
+      console.log('Connected');
+      // You can safely call _send or other methods that use connection.write here
+    });
+  
+    this.connection.on('error', (error) => {
+      console.error('Failed to connect', error);
+      // Handle connection errors
+    });
+  
+    return this;
+  };
+
+/**
+ *  Send a command to the switcher
+ */
+
+Rosstalk.prototype._send = function _send(command) {
+
+  var buf = Buffer.from(command + cr);
+
+  this.connection.write(buf);
+
+  return this;
+};
+
+/**
+ *  Transition an me
+ *
+ *  @param {String} me - the me number
+ *  @param {Boolean} auto - whether or not to auto trans
+ */
+
+Rosstalk.prototype.trans = function trans(me, auto) {
+  me = me || 1;
+
+  var cmd = auto ? 'MEAUTO' : 'MECUT';
+
+  this._send(cmd + ' ' + me);
+};
+
+/**
+ *  Fire a custom control
+ *
+ *  @param {Number} bank - the cc bank number
+ *  @param {Number} num - the cc in the bank to fire
+ */
+
+Rosstalk.prototype.cc = function cc(bank, num) {
+  if (num < 10) num = '0' + num.toString();
+
+  var cmd = 'CC ' + bank+num;
+
+  this._send(cmd);
+};
+
+/**
+ *  Fade to black
+ */
+
+Rosstalk.prototype.ftb = function ftb() {
+  var cmd = 'FTB';
+
+  this._send(cmd);
+};
+
+/**
+ *  Trans a keyer
+ *
+ *  @param {Number} me - the me number
+ *  @param {Number} key - the keyer on the me
+ *  @param {Boolean} auto - auto trans for keyer
+ */
+
+Rosstalk.prototype.transKey = function transKey(me, key, auto) {
+  var cmd = auto ? 'KEYAUTO' : 'KEYCUT';
+
+  cmd += ' ' + me + ':' + key;
+
+  this._send(cmd);
+};
--- src/examples/activeSpeaker.ts
+++ src/examples/activeSpeaker.ts
@@ -1,6 +1,13 @@
 import Zosc from "../Zosc";    // import the class
-import rosstalk from ""
+const Rosstalk = require("../Rosstalk");    // import the class
 let zosc = new Zosc("10.10.80.21", 9090, 1234);    // create a new instance of the ZOSC class
 
 console.log("App Started.");
 
+var switcher = new Rosstalk({
+    host: '10.10.80.51' // switcher ip
+});
+
+switcher.connect();
+
+switcher.trans('ME:PP'); // transition to program

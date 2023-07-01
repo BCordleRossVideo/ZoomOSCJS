bd83b411187eef162e35d1ba009c1f44d02e47e2
Bo <bcordle@rossvideo.com>
2023-06-30 23:54:47 -0400

    activeSpeaker updates

---
+++ src/examples/activeSpeaker.ts
@@ -0,0 +1,6 @@
+import Zosc from "../Zosc";    // import the class
+import rosstalk from ""
+let zosc = new Zosc("10.10.80.21", 9090, 1234);    // create a new instance of the ZOSC class
+
+console.log("App Started.");
+
